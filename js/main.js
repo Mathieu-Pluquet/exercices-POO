5479ef0a17e747b882ae885a4e2e91cfc46c4f5e
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-08-23 16:44:03 +0200

    titre

--- js/main.js
+++ js/main.js
@@ -1,159 +1,171 @@
 // exercice1
-// alert("exercice 1")
-//
-// var user = {
-//     prenom:"John",
-//     nom:"Doe",
-//     pays:"france",
-//     age:50,
-//     carte:["mastercard","visa"],
-//     getName: function() {
-//     alert(this.prenom);
-//   },
-//   getPrenom: function() {
-//     user.getName();
-//   },
-//   getNom: function(){
-//     alert(this.nom);
-//   },
-//   getAge: function(){
-//     alert(2017-this.age)
-//   },
-// };
-// user.getName();
-// user.getNom();
-// user.getAge();
-//
-// var site = {
-//   pays: ["FRANCE","BELGIQUE","ESPAGNE"],
-//   carte:["MASTERCARD","VISA"],
-//   age:17
-// };
-//
-// if (user.age>site.age) {
-//   alert("bienvenue copain")
-// }
-// else {
-//   alert("au revoir copain")
-// }
-//
-// for (i=0; i<site.pays.length;i++) {
-//   if (user.pays.toUpperCase() == site.pays[i]) {
-//     alert("pays ok")
-//   }
-// }
-// var compt=0
-// for (i=0; i < user.carte.length; i++)
-// {
-//     for (j=0; j < site.carte.length; j++)
-//     {
-//         if (user.carte[i].toUpperCase() == site.carte[j].toUpperCase())
-//         {
-//           compt++
-//         }
-//     }
-// }
-// if (compt>0){
-//   alert("gg carte")
-// }
-//
+alert("exercice 1")
+
+var user = {
+    prenom:"John",
+    nom:"Doe",
+    pays:"france",
+    age:50,
+    carte:["mastercard","visa"],
+    getName: function() {
+    alert(this.prenom);
+  },
+  getPrenom: function() {
+    user.getName();
+  },
+  getNom: function(){
+    alert(this.nom);
+  },
+  getAge: function(){
+    alert(2017-this.age)
+  },
+};
+user.getName();
+user.getNom();
+user.getAge();
+
+var site = {
+  pays: ["FRANCE","BELGIQUE","ESPAGNE"],
+  carte:["MASTERCARD","VISA"],
+  age:17
+};
+
+if (user.age>site.age) {
+  alert("bienvenue copain")
+}
+else {
+  alert("au revoir copain")
+}
+
+for (i=0; i<site.pays.length;i++) {
+  if (user.pays.toUpperCase() == site.pays[i]) {
+    alert("pays ok")
+  }
+}
+var compt=0
+for (i=0; i < user.carte.length; i++)
+{
+    for (j=0; j < site.carte.length; j++)
+    {
+        if (user.carte[i].toUpperCase() == site.carte[j].toUpperCase())
+        {
+          compt++
+        }
+    }
+}
+if (compt>0){
+  alert("gg carte")
+}
+
 
 // exercice2
-// alert("exercice 2")
-//
-// function combattant (nom, atk, def, hp) {
-//   this.nom=nom;
-//   this.atk=atk;
-//   this.def=def;
-//   this.hp=hp;
-//   this.getName = function(adv) {
-//   alert(adv.nom + " attaque " + this.nom + " il lui reste " + (this.hp=(this.hp - adv.atk)) + " hp");
-//  };
-// }
-//
-// function magicien (nom, atk, def, hp, mana) {
-//   this.nom=nom;
-//   this.atk=atk;
-//   this.def=def;
-//   this.hp=hp;
-//   this.mana=mana;
-//
-//   this.getmagie= function() {
-//     if (mana<10) {
-//       alert("tu ne peut pas te heal")
-//     }
-//     else {
-//       alert(nom + " se soigne et a " + (this.mana-10 && this.hp+10) + " hp");
-//     }
-//   }
-//   this.getmage = function(adv) {
-//   alert(adv.nom + " attaque " + nom + " il lui reste " + (this.hp=(this.hp - adv.atk)) + " hp")
-//  };
-//  this.getatk = function(adv) {
-//    alert(nom + " attaque " + adv.nom + " il lui reste " + (adv.hp=(adv.hp - this.atk)) + " hp")
-//  }
-// };
-//
-// var aaron = new combattant("aaron",50,100,1000);
-// var ben = new combattant("ben",100,100,1500);
-// var math = new magicien("math",100,100,800,100);
-//
-// ben.getName(aaron);
-// aaron.getName(ben);
-// math.getmage(ben);
-// math.getmage(aaron);
-// math.getmagie();
-// math.getatk(ben);
+alert("exercice 2")
+
+function combattant (nom, atk, def, hp) {
+  this.nom=nom;
+  this.atk=atk;
+  this.def=def;
+  this.hp=hp;
+  this.getName = function(adv) {
+  alert(adv.nom + " attaque " + this.nom + " il lui reste " + (this.hp=(this.hp - adv.atk)) + " hp");
+ };
+}
+
+function magicien (nom, atk, def, hp, mana) {
+  this.nom=nom;
+  this.atk=atk;
+  this.def=def;
+  this.hp=hp;
+  this.mana=mana;
+
+  this.getmagie= function() {
+    if (mana<10) {
+      alert("tu ne peut pas te heal")
+    }
+    else {
+      alert(nom + " se soigne et a " + (this.mana-10 && this.hp+10) + " hp");
+    }
+  }
+  this.getmage = function(adv) {
+  alert(adv.nom + " attaque " + nom + " il lui reste " + (this.hp=(this.hp - adv.atk)) + " hp")
+ };
+ this.getatk = function(adv) {
+   alert(nom + " attaque " + adv.nom + " il lui reste " + (adv.hp=(adv.hp - this.atk)) + " hp")
+ }
+};
+
+var aaron = new combattant("aaron",50,100,1000);
+var ben = new combattant("ben",100,100,1500);
+var math = new magicien("math",100,100,800,100);
+
+ben.getName(aaron);
+aaron.getName(ben);
+math.getmage(ben);
+math.getmage(aaron);
+math.getmagie();
+math.getatk(ben);
 
 
 
 // exercice3
+alert("exercice 3")
 
-// var tableauProduct = [];
-// var i =0;
-// function produit (titre,phrase,description) {
-//   this.titre=titre;
-//   this.phrase=phrase;
-//   this.description=description;
-// };
-//
-// function form (titre,phrase,description) {
-//   this.titre=titre;
-//   this.phrase=phrase;
-//   this.description=description;
-//   this.createProduct=function() {
-//   // new Produit et mettre dand tableauProduct
-//     tableauProduct[i] = new produit (this.titre,this.phrase,this.description)
-//     var envoie = $('<p class="lol" ></p>').text(tableauProduct[i].titre);
-//     var envoie2 = $('<p class="lol" ></p>').text(tableauProduct[i].phrase);
-//     var envoie3 = $('<p class="lol" ></p>').text(tableauProduct[i].description);
-//     var envoie4 = $('<p class="test" ></p>').text(" ");
-//     $(".bg").append(envoie);
-//     $(".bg").append(envoie2);
-//     $(".bg").append(envoie3);
-//     $(".bg").append(envoie4);
-//     $('#mess1').val("")
-//     $('#mess2').val("")
-//     $('#mess3').val("")
-//     i++;
-//   }
-// };
-//
-// $('#boutton').on("click",function() {
-//   var formulaire = new form ($("#mess1").val(), $("#mess2").val(), $("#mess3").val())
-//   formulaire.createProduct()
-// });
+var tableauProduct = [];
+var i =0;
+var envoie, envoie2, envoie3, envoie4;
+function produit (titre,phrase,description) {
+  this.titre=titre;
+  this.phrase=phrase;
+  this.description=description;
+};
+
+function reset () {
+  $('#mess1').val("")
+  $('#mess2').val("")
+  $('#mess3').val("")
+}
+
+function test() {
+  $(".bg").append(envoie);
+  $(".bg").append(envoie2);
+  $(".bg").append(envoie3);
+  $(".bg").append(envoie4);
+}
+
+function form (titre,phrase,description) {
+  this.titre=titre;
+  this.phrase=phrase;
+  this.description=description;
+  this.createProduct=function() {
+  // new Produit et mettre dand tableauProduct
+    tableauProduct[i] = new produit (this.titre,this.phrase,this.description)
+    envoie = $('<p class="lol" ></p>').text(tableauProduct[i].titre);
+    envoie2 = $('<p class="lol" ></p>').text(tableauProduct[i].phrase);
+    envoie3 = $('<p class="lol" ></p>').text(tableauProduct[i].description);
+    envoie4 = $('<p class="test" ></p>').text(" ");
+    test();
+    reset();
+    i++;
+  }
+};
+
+$('#boutton').on("click",function() {
+  var formulaire = new form ($("#mess1").val(), $("#mess2").val(), $("#mess3").val())
+  formulaire.createProduct()
+});
 
 
 // exercice4
+$.getJSON('test.json',function(data){
 
+           $.each(data,function(index,d){
 
-var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML =
-            this.responseText;
-       }
-    };
-    xhttp.open("GET", "test.txt", true);
-    xhttp.send();
+             $('#demo').append('Prénom : ' + d.Prenom + '<br>');
+
+             $('#demo').append('Nom : ' + d.Nom + '<br>');
+
+             $('#demo').append('Age : ' + d.Age + '<br><br>');
+
+           });
+
+         });
